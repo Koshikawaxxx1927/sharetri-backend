86077f0a584ad88abf35cd759a5ea5982c5e04e0
Koshikawaxxx1927 <ebi1927@icloud.com>
2024-02-26 15:18:47 +0900

    I used lint

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"log"
 	"github.com/Koshikawaxxx1927/sharetri-backend/routes"
 	"github.com/Koshikawaxxx1927/sharetri-backend/config"
 	"github.com/Koshikawaxxx1927/sharetri-backend/app/models"
@@ -15,5 +16,7 @@ func main() {
 	config.InitDB(*env, true, models.Trip{}, models.User{}, models.Prefecture{}, models.Spot{})
 	seed.Seed()
 	router := routes.Router()
-	router.Run(":8080")
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal("Failed to build server")
+	}
 }
